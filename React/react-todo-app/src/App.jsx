7ea46c1901c96b878ddce05b7188a13008df6323
Noh <nhw3152@gmail.com>
2025-07-04 17:45:35 +0900

    Form 컴포넌트 추가

--- React/react-todo-app/src/App.jsx
+++ React/react-todo-app/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
 import {useState} from 'react';
+import Lists from './components/lists';
+import Form from './components/Form';
 
 export default function App() {
 
@@ -18,57 +20,17 @@ export default function App() {
 
   const [value, setValue] = useState('');
 
-  const btnStyle = {
-    color: '#fff',
-    border: 'none',
-    padding: '5px 9px',
-    borderRadius: '50%',
-    cursor: 'pointer',
-    float: 'right'
-  }
-
-  const getStyle = (completed) => {
-    return {
-      padding: '10px',
-      borderBottom: '1px #ccc dotted',
-      textDecoration: completed ? 'line-through' : 'none'
-    }
-  }
-
-  const handleClick = (id) => {
-    console.log(id);
-    let newTodoData = todoData.filter((data) => data.id !== id);
-    console.log(newTodoData);
-    setTodoData(newTodoData);
-  }
-
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setValue(e.target.value);
-  }
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    
+
     let newTodo = {
-      id : Date.now(),
-      title : value,
-      completed : false,
+    id : Date.now(),
+    title : value,
+    completed : false,
     }
 
-    setTodoData([...todoData, newTodo]);
-    setValue('');
-  }
-
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    })
-
-    setTodoData(newTodoData);
+  setTodoData([...todoData, newTodo]);
+  setValue('');
   }
 
   return (
@@ -78,35 +40,16 @@ export default function App() {
           <h1>할 일 목록</h1>
         </div>
 
-    {
-      todoData.map((data) => (
-        <div key={data.id} style={getStyle(data.completed)}>
-          <input type='checkbox' 
-          onChange={() => handleCompleteChange(data.id)} 
-          checked={data.completed}/>
-          {data.title}
-          <button style={btnStyle} onClick={() => handleClick(data.id)}>X</button>
-        </div>
-      ))
-    }
+    <Lists 
+    todoData={todoData}
+    setTodoData={setTodoData}
+    />
 
-    <form style={{display: 'flex'}} onSubmit={handleSubmit}>
-      <input 
-      type='text'
-      name='value'
-      style={{flex: '10', padding: '5px'}}
-      placeholder='할 일을 입력하시오.'
-      value={value}
-      onChange={handleChange}
-      />
-
-      <input
-      type='submit'
-      value='입력'
-      className='btn'
-      style={{flex: '10px'}}
-      />
-    </form>
+    <Form 
+    handleSubmit={handleSubmit}
+    value={value}
+    setValue={setValue}
+    />
     
       </div>
     </div>
---
+++ React/react-todo-app/src/components/Form.jsx
@@ -0,0 +1,27 @@
+export default function Form({handleSubmit, value, setValue}) {
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        setValue(e.target.value)
+    }
+
+    return <div>
+        <form style={{display: 'flex'}} onSubmit={handleSubmit}>
+            <input 
+                type='text'
+                name='value'
+                style={{flex: '10', padding: '5px'}}
+                placeholder='할 일을 입력하시오.'
+                value={value}
+                onChange={handleChange}
+            />
+
+            <input
+                type='submit'
+                value='입력'
+                className='btn'
+                style={{flex: '10px'}}
+            />
+        </form>
+    </div>;
+}
