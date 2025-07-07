c21fb0d40e8b07fcb9b2b437b56a0e45093ab9db
Noh <nhw3152@gmail.com>
2025-07-07 13:38:20 +0900

    axios 생성

--- React/react-netflix/src/api/axios.js
+++ React/react-netflix/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL : "https://api.themoviedb.org/3",
     params : {
-        api_key : "bb39187cc49768621786e3240d16cf65",
+        api_key : "",
         language : "ko-KR",
     },
 });
