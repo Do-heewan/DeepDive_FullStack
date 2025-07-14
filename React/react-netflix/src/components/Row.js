5d7224b39e6d85c56aa4cc2a4b0924c910caa196
Noh <nhw3152@gmail.com>
2025-07-14 16:08:23 +0900

    Movie 모달 UI 생성

--- React/react-netflix/src/components/Row.js
+++ React/react-netflix/src/components/Row.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react'
 import requests from '../api/requests';
 import axios from '../api/axios';
 import './Row.css';
+import MovieModal from './MovieModal';
 
 export default function Row({title, id, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [movieSelected, setMovieSelected] = useState({});
 
     useEffect(() => {
         fetchMovieData();
@@ -15,6 +18,11 @@ export default function Row({title, id, fetchUrl, isLargeRow}) {
         const requests = await axios.get(fetchUrl);
         // console.log(`request`, requests);
         setMovies(requests.data.results);
+    };
+
+    const handleClick = (movie) => {
+      setModalOpen(true);
+      setMovieSelected(movie);
     };
 
   return (
@@ -37,6 +45,7 @@ export default function Row({title, id, fetchUrl, isLargeRow}) {
                     className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                     src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path: movie.backdrop_path} `}
                     alt={movie.name}
+                    onClick={() => handleClick(movie)}
                 />
             ))}
         </div>
@@ -52,7 +61,9 @@ export default function Row({title, id, fetchUrl, isLargeRow}) {
         </div>
       </div>
 
-
+            {modalOpen && 
+              <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+            }
 
     </section>
   )
