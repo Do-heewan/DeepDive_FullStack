5ab445e8197ea41234d091750773d197ac73ff04
Noh <nhw3152@gmail.com>
2025-07-14 13:20:16 +0900

    비디오 배너 컴포넌트 영상 임베드 추가

--- React/react-netflix/src/components/Banner.js
+++ React/react-netflix/src/components/Banner.js
@@ -38,7 +38,7 @@ export default function Banner() {
             <header
         className='banner'
         style={{
-            backgroundImage : `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`,
+            backgroundImage : `url("http://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
             backgroundPosition : "top center",
             backgroundSize : "cover",
         }}
@@ -61,11 +61,39 @@ export default function Banner() {
     else {
         return (
             <Container>
-                <HomeContainer>clicked</HomeContainer>
+                <HomeContainer>
+                    <Iframe 
+                        width="640" 
+                        height="360" 
+                        src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`} 
+                        title="YouTube video player" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" 
+                        allowfullscreen>
+                    </Iframe>
+                </HomeContainer>
             </Container>
         );
     }
 }
+
+const Iframe = styled.iframe`
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    opacity: 0.65;
+    border: none;
+
+    &::after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+`
 
 const Container = styled.div`
     display: flex;
