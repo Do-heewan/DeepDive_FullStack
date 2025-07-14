4f26d0ffcb4aab0e3f16c2981be21551b11bac14
Noh <nhw3152@gmail.com>
2025-07-14 15:03:15 +0900

    비디오 행 컴포넌트

--- React/react-netflix/src/components/Row.js
+++ React/react-netflix/src/components/Row.js
@@ -1,1 +1,59 @@
-ㅁㄴㅇ
+import React, { useState, useEffect } from 'react'
+import requests from '../api/requests';
+import axios from '../api/axios';
+import './Row.css';
+
+export default function Row({title, id, fetchUrl, isLargeRow}) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetchMovieData();
+
+    }, []);
+
+    const fetchMovieData = async () => {
+        const requests = await axios.get(fetchUrl);
+        // console.log(`request`, requests);
+        setMovies(requests.data.results);
+    };
+
+  return (
+    <section className='row'>
+      <h2>{title}</h2>
+      <div className='slider'>
+        <div className='slider__arrow-left'>
+          <span 
+            className='arrow'
+            onClick={() => {
+                document.getElementById(id).scrollLeft -= window.innerWidth - 20;
+            }}>
+            {"<"}
+          </span>
+        </div>
+        <div id={id} className='row__posters'>
+            {movies.map(movie => (
+                <img
+                    key={movie.id}
+                    className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                    src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path: movie.backdrop_path} `}
+                    alt={movie.name}
+                />
+            ))}
+        </div>
+        <div className='slider__arrow-right'>
+            <span 
+            className='arrow'
+            onClick={() => {
+                document.getElementById(id).scrollLeft += window.innerWidth - 20;
+                console.log(`asd`)
+            }}>
+                {">"}
+            </span>
+        </div>
+      </div>
+
+
+
+    </section>
+  )
+}
